73e2cb21edd7e6877c295e81ecc134f4a4725915
Seth Lachman <lachmanseth17@gmail.com>
2019-05-29 15:25:59 -0400

    Added middleware logger.

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require("express");
 
 const server = express();
 
+server.use(logger);
+
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+    `Request method: ${req.method}, 
+      Request URL: ${req.originalUrl}, 
+      Date: ${new Date()}`
+  );
+  next();
+}
 
 module.exports = server;
